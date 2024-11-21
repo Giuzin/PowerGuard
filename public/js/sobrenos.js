918ffbd3581e5269899d11a9fbc0b8b18de4fada
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-11-21 19:28:08 -0300

    refactor: alterações feitas no sobrenos e na tela de login

--- public/js/sobrenos.js
+++ public/js/sobrenos.js
@@ -1,3 +1,8 @@
 function redirectToIndex() {
     window.location.href = 'index.html';
 }
+
+function showSection(sectionId) {
+    document.querySelectorAll('.content-section').forEach(section => section.style.display = 'none');
+    document.getElementById(sectionId).style.display = 'flex';
+}
